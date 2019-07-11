1b2ce3ecc560f97178a8f4e3ed040a0bcf8718fa
LeoKnox <LeoKnox@users.noreply.github.com>
2019-07-10 18:30:17 -0700

    Add files via upload
    Filling out main window of app.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Head from './Head';
 import Side from './Side';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <Head />
       <Side />
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div className="Main">
+            <svg width="20%" height="80%">
+                <rect width="300" height="100" />
+            </svg>
+        </div>
+    )
+}
+
+export default Main;

fb1e3630ab3fc90ba5297b60dbd2ba75269ad1b6
LeoKnox <LeoKnox@users.noreply.github.com>
2019-07-11 11:44:41 -0700

    Add files via upload
    Set form to use data onchange.

--- src/Side.js
+++ src/Side.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
 
-class Side extends React.Component {
+class Side extends Component {
     constructor() {
         super()
+        this.state={
+            roomName: "",
+            height: "",
+            width: ""
+        }
+        this.readValue = this.readValue.bind(this);
     }
+
+    readValue(event) {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         return (
             <form id="dungeonStats">
                 <p>Room Name:
-                    <input type="text" />
+                    <input
+                        type="text"
+                        value={this.state.roomName}
+                        name="roomName"
+                        onChange={this.readValue}
+                    />
                 </p>
                 <p>Width:
-                    <input type="text" />
+                    <input
+                        type="text"
+                        placeholder="5"
+                        value={this.state.firstName}
+                        name="width"
+                        onChange={this.readValue}
+                    />
                 </p>
                 <p>Height:
-                    <input type="text" />
+                    <input type="text" placeholder="3"/>
                 </p>
                 <p>Doors:
                     <input type="text" />
                 </p>
                 <button>Update</button>
+                <h1>{this.state.roomName}</h1>
             </form>
         )
     }
